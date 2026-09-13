c04a1337d8dbb69c2e3a3ece28013c3498ff10e3
azamzaimramadhan2809 <azamzaimramadhan2809@gmail.com>
2026-09-13 10:47:30 +0700

    kurang verifikasi brevo

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -4,7 +4,10 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\ServiceProvider;
+use Symfony\Component\HttpClient\NativeHttpClient;
+use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoApiTransport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +24,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Mail::extend('brevo', function (array $config): BrevoApiTransport {
+            return new BrevoApiTransport(
+                (string) $config['key'],
+                new NativeHttpClient,
+            );
+        });
+
         ResetPassword::createUrlUsing(function (User $user, string $token): string {
             $frontendUrl = rtrim((string) config('app.frontend_url'), '/');
 
